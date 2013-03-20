b6fb090ef733f999c3bf0a308cbb10fc14809962
omar <omarisel01@gmail.com>
2013-03-20 13:59:30 -0600

    agregando backbone y terminando de validar los datos con regular exp

--- app_signal/media/js/core/app.js
+++ app_signal/media/js/core/app.js
@@ -1,74 +1,30 @@
-//Models are used to represent data from your server and actions you perform on 
-//them will be translated to RESTful operations.
+jQuery(document).ready(function($) {
+	
+	bits = $(".bits_entree");
+
+	
 
 
- var App = Backbone.Model.extend({
+	$(".modular").bind("click",function(){
 
- 		defaults:{
-
-         version: "1.0.0",
-         root: '' ,
-         signal: []
-
-      } ,
-
- 	// triggered whenever you create a new instance of a model
- 	initialize : function () {
-
-
- 		this.on("change:signal",function(model){
+		bits_entree = $(bits).val();
  
-
- 			var signal = model.get("signal");
-
- 			 
- 			console.log("gooooooo");
+		EntradaBinaria.validar(bits_entree);
 
 
- 		});
- 		
-
- 		 
-
- 	},
-
-       set_signal : function(name_attr){
-
-/*
-NOTE: According to the Underscore API, _.clone() copies certain 
-nested items by reference. The root/parent object is cloned 
-though, so it will work fine for backbone. That is, if your 
-array is very simple and does not have nested structures 
-e.g. [1, 2, 3].
-
-*/    
-
-	 	 before_data = _.clone(this.get('signal'));
-
- 		 before_data.push(name_attr);
-
-         this.set({ signal : before_data });
- 
-
-      }
-
-
- }); // end extends Backbone model
+	})
 
 
 
- // instance app
+// borrar datos
+
+	$(".borrar").bind("click",function(){
 
 
- var app = new App;
-
- 
- app.set_signal("0101010220201");
-
-  app.set_signal("01019210101");
+		$(bits).val("");
 
 
- 
+	})
 
-
+});
 
